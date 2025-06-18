629250f452c9506574328e6b27b0a167d660f7b6
Mike5493 <botw5493@gmail.com>
2025-06-18 12:09:08 -0500

    Silly shootemup game inspired by Berzerk!

---
+++ main.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"math/rand"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+const (
+	screenWidth  = 1280
+	screenHeight = 720
+)
+
+type Player struct {
+	Pos    rl.Vector2
+	Radius float32
+	Speed  float32
+}
+
+type Bullet struct {
+	Pos    rl.Vector2
+	Vel    rl.Vector2
+	Active bool
+}
+
+type Enemy struct {
+	Pos    rl.Vector2
+	Radius float32
+	Speed  float32
+	Active bool
+}
+
+func main() {
+	rl.InitWindow(screenWidth, screenHeight, "~CHOOTEMUP~")
+	rl.SetTargetFPS(60)
+
+	player := Player{
+		Pos:    rl.NewVector2(screenWidth/2, screenHeight/2),
+		Radius: 16,
+		Speed:  300,
+	}
+
+	bullets := make([]Bullet, 50)
+	for i := range bullets {
+		bullets[i] = Bullet{Active: false}
+	}
+
+	enemies := make([]Enemy, 20)
+	for i := range enemies {
+		enemies[i] = Enemy{Active: false}
+	}
+
+	for !rl.WindowShouldClose() {
+		// Handle input and movement
+		if rl.IsKeyDown(rl.KeyW) {
+			player.Pos.Y -= player.Speed * rl.GetFrameTime()
+		}
+		if rl.IsKeyDown(rl.KeyS) {
+			player.Pos.Y += player.Speed * rl.GetFrameTime()
+		}
+		if rl.IsKeyDown(rl.KeyA) {
+			player.Pos.X -= player.Speed * rl.GetFrameTime()
+		}
+		if rl.IsKeyDown(rl.KeyD) {
+			player.Pos.X += player.Speed * rl.GetFrameTime()
+		}
+
+		// Shooting
+		if rl.IsKeyPressed(rl.KeyUp) {
+			fireBullet(&bullets, player.Pos, rl.NewVector2(0, -1))
+		}
+		if rl.IsKeyPressed(rl.KeyDown) {
+			fireBullet(&bullets, player.Pos, rl.NewVector2(0, 1))
+		}
+		if rl.IsKeyPressed(rl.KeyLeft) {
+			fireBullet(&bullets, player.Pos, rl.NewVector2(-1, 0))
+		}
+		if rl.IsKeyPressed(rl.KeyRight) {
+			fireBullet(&bullets, player.Pos, rl.NewVector2(1, 0))
+		}
+
+		// Update bullets
+		for i := range bullets {
+			if bullets[i].Active {
+				bullets[i].Pos.X += bullets[i].Vel.X * 400 * rl.GetFrameTime()
+				bullets[i].Pos.Y += bullets[i].Vel.Y * 400 * rl.GetFrameTime()
+				if bullets[i].Pos.X < 0 || bullets[i].Pos.X > screenWidth || bullets[i].Pos.Y < 0 || bullets[i].Pos.Y > screenHeight {
+					bullets[i].Active = false
+				}
+			}
+		}
+
+		// Spawn enemies
+		if rand.Float32() < 0.02 {
+			spawnEnemy(&enemies)
+		}
+
+		// Update Enemies
+		for i := range enemies {
+			if enemies[i].Active {
+				dir := rl.Vector2Subtract(player.Pos, enemies[i].Pos)
+				dir = rl.Vector2Normalize(dir)
+				enemies[i].Pos.X += dir.X * enemies[i].Speed * rl.GetFrameTime()
+				enemies[i].Pos.Y += dir.Y * enemies[i].Speed * rl.GetFrameTime()
+			}
+		}
+
+		// Draw
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.Black)
+		rl.DrawCircleV(player.Pos, player.Radius, rl.DarkGreen)
+		for _, bullet := range bullets {
+			if bullet.Active {
+				rl.DrawRectangle(int32(bullet.Pos.X-4), int32(bullet.Pos.Y-4), 8, 8, rl.Yellow)
+			}
+		}
+		for _, enemy := range enemies {
+			if enemy.Active {
+				rl.DrawCircleV(enemy.Pos, enemy.Radius, rl.Red)
+			}
+		}
+
+		rl.EndDrawing()
+	}
+
+	rl.CloseWindow()
+}
+
+func fireBullet(bullets *[]Bullet, pos rl.Vector2, dir rl.Vector2) {
+	for i := range *bullets {
+		if !(*bullets)[i].Active {
+			(*bullets)[i] = Bullet{
+				Pos:    pos,
+				Vel:    dir,
+				Active: true,
+			}
+			break
+		}
+	}
+}
+
+func spawnEnemy(enemies *[]Enemy) {
+	for i := range *enemies {
+		if !(*enemies)[i].Active {
+			var pos rl.Vector2
+			side := rand.Intn(4)
+			switch side {
+			case 0: // Top
+				pos = rl.NewVector2(float32(rand.Intn(screenWidth)), 0)
+			case 1: // Bottom
+				pos = rl.NewVector2(float32(rand.Intn(screenWidth)), screenHeight)
+			case 2: // Left
+				pos = rl.NewVector2(0, float32(rand.Intn(screenHeight)))
+			case 3: // Right
+				pos = rl.NewVector2(screenWidth, float32(rand.Intn(screenHeight)))
+			}
+			(*enemies)[i] = Enemy{
+				Pos:    pos,
+				Radius: 16,
+				Speed:  100,
+				Active: true,
+			}
+			break
+		}
+	}
+}
